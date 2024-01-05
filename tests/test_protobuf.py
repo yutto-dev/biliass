4fa8b4dc641f57d8848d0cc7ca037bc5534005f0
SigureMo <sigure.qaq@gmail.com>
2024-01-06 01:58:09 +0800

    ✏️ fix: fix test case name

--- tests/test_protobuf.py
+++ tests/test_protobuf.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 from biliass import ReadCommentsBilibiliProtobuf
 
 
-def test_xml_v1():
+def test_protobuf():
     with open("./tests/test_files/test.pb", "rb") as f:
         ReadCommentsBilibiliProtobuf(f.read(), 10)
